b7fc3e91d7954a9ba681998e65954a9e7c4457f1
Wilmorecn <wilmorecn@gmail.com>
2020-01-08 20:37:05 -0600

    update html and app.py files

--- Website/app.py
+++ Website/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, render_template, send_file, request
-from flask_pymongo import PyMongo
 from keras.models import load_model
 from keras import backend as K
 import pickle
@@ -12,41 +11,51 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# @app.route("/model", methods = ['POST', 'GET'])
+# def model():
+#     errors = []
+#     results = []
+#     user_input = '______'
+#     if request.method == 'POST':
+#         try:
+#             user_input = request.form['user_input']
+#             print(user_input)
+#         except Exception as e:
+#             errors.append("it didn't work...")
 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/election"
-# mongo = PyMongo(app)
+#     return render_template('model.html', errors = errors, results = results, text = user_input)
 
-# @app.route("/")
-# def index():
-#     #.find is the query of data to display to the user
-#     display_fips = mongo.db.election.find_one()
-#     #example database query
-#     test_query = mongo.db.election.find({'FIPS':{'$eq':'01043'}})
-#     list1 = []
-#     for x in test_query:
-#         data = {}
-#         data.update({
-#         'FIPS': x['FIPS'],
-#         'Household_Income': x['Household Median Income']
-#         })
-#         list1.append(data)
-#     print(list1)
-#     return render_template("index.html", display_fips=display_fips)
+# @app.route('/predict', methods = ['POST', 'GET'])
+# def predict():
+#     user_input = request.form.get('url')
+#     categories = ['Single Malt Scotch',
+#                   'Flavored Whiskey and Liqueurs',
+#                   'Bourbon',
+#                   ' Blended Malt Scotch Whisky ',
+#                   'Blended Scotch Whisky',
+#                   'Blended Whiskey (Multi-country)',
+#                   ' Single Malt Whisky (Multi-country)',
+#                   ' Single Grain Whisky (Multi-country)',
+#                   'Japan',
+#                   ' Canada',
+#                   'Irish',
+#                   'Generic Whisky (Multi-country)',
+#                   'Rye Whisky',
+#                   'White Whisky',
+#                   'Craft Whisky']
+#     with open('static/models/tfidf.pickle', 'rb') as f:
+#         vectorizer = pickle.load(f)
+#     K.clear_session()
+#     keras_model = load_model('static/models/keras_model.h5')
+#     keras_model._make_predict_function()
+#     v_input = vectorizer.transform([user_input])
+#     predictions = keras_model.predict(v_input)
+#     max_index = np.argmax(predictions)
+#     output = categories[max_index]
 
-
-#@app.route("/scrape")
-# def scraper():
-#     display_mars = mongo.db.display_mars
-#     display_mars_data = scrape_mars.scrape()
-#     display_mars.update({}, display_mars_data, upsert=True)
-#     return redirect("/", code=302)   
-
-
-
-
-
-
+    return render_template('index.html', text=output)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # load_keras_model()
+    app.run()
