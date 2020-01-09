5f3b28d4f82d4d0a11f0749109b2789c5df3e40c
ricochet <eashaw8@gmail.com>
2020-01-08 19:42:10 -0600

    app file

--- Website/app.py
+++ Website/app.py
@@ -1,31 +1,37 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, jsonify, render_template, send_file, request
 from flask_pymongo import PyMongo
+from keras.models import load_model
+from keras import backend as K
+import pickle
+import numpy as np
 
 
 app = Flask(__name__)
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/election"
-mongo = PyMongo(app)
-
-
-
-
-
+# render out an index page
 @app.route("/")
-def index():
-    #.find is the query of data to display to the user
-    display_fips = mongo.db.election.find_one()
-    test_query = mongo.db.election.find({'FIPS':{'$eq':'01043'}})
-    list1 = []
-    for x in test_query:
-        data = {}
-        data.update({
-        'FIPS': x['FIPS'],
-        'Household_Income': x['Household Median Income']
-        })
-        list1.append(data)
-    print(list1)
-    return render_template("index.html", display_fips=display_fips)
+def home():
+    return render_template("index.html")
+
+
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/election"
+# mongo = PyMongo(app)
+
+# @app.route("/")
+# def index():
+#     #.find is the query of data to display to the user
+#     display_fips = mongo.db.election.find_one()
+#     #example database query
+#     test_query = mongo.db.election.find({'FIPS':{'$eq':'01043'}})
+#     list1 = []
+#     for x in test_query:
+#         data = {}
+#         data.update({
+#         'FIPS': x['FIPS'],
+#         'Household_Income': x['Household Median Income']
+#         })
+#         list1.append(data)
+#     print(list1)
+#     return render_template("index.html", display_fips=display_fips)
 
 
 #@app.route("/scrape")
